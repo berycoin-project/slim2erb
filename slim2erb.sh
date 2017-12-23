efe8d14a36368477dc4f6fdc0f873771a2cff8cd
Berycoin Project <32966245+berycoin-project@users.noreply.github.com>
2017-12-23 15:24:11 +0500

    Update slim2erb.sh

--- slim2erb.sh
+++ slim2erb.sh
@@ -1,4 +1,5 @@
 for x in $(find . -name \*.slim); do
-  slimrb --rails -e -p $x $(echo "$x" | sed 's/\.slim$/.erb/')
+  slimrb --rails -e $x $(echo "$x" | sed 's/\.slim$/.erb/')
+  echo $x
 done
 find . -name "*.slim" -type f -delete
